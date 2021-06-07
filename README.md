ba234044e162cfde4610bf5c4f68c04cf21595a6
Sina Dashti <sina.v.dashti@gmail.com>
2021-06-07 23:09:19 +0200

    added new features

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 # README
 
----
-
 This project idea comes from [`The Odin Project`](https://www.theodinproject.com/paths) web development **Foundation** path.
 
 The goal hear is understanding the `DOM` manipulation with `JavaScript`.
 
-[live demo](https://sinadashti.github.io/etch-a-sketch/)
+[Click to sketch](https://sinadashti.github.io/etch-a-sketch/)
--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 let container = document.getElementById('container');
+let mode = Array.from(document.getElementsByName('mode'));
+let containerSize = document.getElementById('size');
 const resetButton = document.getElementById("resetBtn");
 const sizeButton = document.getElementById("sizeBtn");
 let gridSize = 16;
 
-let containerStyle = function(cnt, gSize) {
+let containerStyle = function (cnt, gSize) {
     cnt.style.gridTemplateRows = `repeat(${gSize}, 1fr)`;
     cnt.style.gridTemplateColumns = `repeat(${gSize}, 1fr)`;
 }
 
-const gridCreate = function(cnt, gSize, first = true) {
+const gridCreate = function (cnt, gSize, first = true) {
     for (let i = 0; i < gSize * gSize; i++) {
         let squeare = document.createElement('div');
         cnt.appendChild(squeare);
@@ -23,17 +25,50 @@ const gridCreate = function(cnt, gSize, first = true) {
     }
 }
 
-const removeAll = function(cnt) {
+const removeAll = function (cnt) {
     while (cnt.firstChild) {
         cnt.removeChild(cnt.lastChild);
     }
 }
 
+
+const valueOff = function (e) {
+    if (e.key == 'Control') {
+        container.setAttribute('value', 'off');
+        document.getElementById('freeze').checked = true;
+    }
+    else if (e.key == 'Shift') {
+        // container.getAttribute('value') == 'on' && 
+        if (container.getAttribute('value') == 'on' && container.getAttribute('data-delete') == 'off') {
+            container.setAttribute('data-delete', 'on');
+            document.getElementById('erase').checked = true;
+        }
+    };
+}
+
+const valueOn = function (e) {
+    document.getElementById('sketch').checked = true;
+    if (e.key == 'Control') {
+        container.setAttribute('value', 'on');
+    }
+    else if (e.key == 'Shift') {
+        if (container.getAttribute('value') == 'on' && container.getAttribute('data-delete') == 'on') {
+            container.setAttribute('data-delete', 'off');
+        }
+    };
+}
+
+
 const handleMousemove = (e) => {
-    let r = Math.floor(Math.random() * 255);
-    let g = Math.floor(Math.random() * 255);
-    let b = Math.floor(Math.random() * 255);
-    e.target.style.backgroundColor = `rgb(${r},${g},${b})`;
+    if (container.getAttribute('value') == 'on') {
+        let r = Math.floor(Math.random() * 255);
+        let g = Math.floor(Math.random() * 255);
+        let b = Math.floor(Math.random() * 255);
+        e.target.style.backgroundColor = `rgb(${r},${g},${b})`;
+    }
+    if (container.getAttribute('data-delete') == 'on') {
+        e.target.style.backgroundColor = `hsl(0, 0%, 100%)`;
+    }
 };
 
 
@@ -42,14 +77,31 @@ const doublClickAction = (e) => {
 }
 
 
-const resetAction = function() {
+const resetAction = function () {
     container.childNodes.forEach(e => {
         e.style.backgroundColor = `hsl(0, 0%, 100%)`;
     });
+    document.getElementById('sketch').checked = true;
 }
 
 
-const promptAction = function() {
+const selectMode = function (e) {
+    if (e.target.value == 'sketch') {
+        container.setAttribute('data-delete', 'off');
+        container.setAttribute('value', 'on')
+    }
+    if (e.target.value == 'erase') {
+        container.setAttribute('data-delete', 'on');
+    }
+    if (e.target.value == 'freeze') {
+        container.setAttribute('value', 'off');
+        container.setAttribute('data-delete', 'off');
+    }
+
+}
+
+
+const promptAction = function () {
     let answer = parseInt(prompt('Please choose an integer between bigger than 0 and less than 65'), 10);
     if (answer == null || isNaN(answer) || answer < 0) {
         alert('You should choose a postive integer!');
@@ -60,15 +112,22 @@ const promptAction = function() {
     } else return answer;
 }
 
-const changeSizeAction = function() {
-    gridSize = promptAction();
+const newRestart = function (size) {
     removeAll(container);
-    containerStyle(container, gridSize);
-    gridCreate(container, gridSize, false);
+    containerStyle(container, size);
+    gridCreate(container, size, false);
     addEventsToContainer(container);
+    document.getElementById('rangeSize').innerText = `${size} x ${size}`;
+    document.getElementById('size').value = `${size}`;
+    document.getElementById('sketch').checked = true;
 }
 
-const addEventsToContainer = function(cnt) {
+const changeSizeAction = function () {
+    gridSize = promptAction();
+    newRestart(gridSize);
+}
+
+const addEventsToContainer = function (cnt) {
     cnt.childNodes.forEach(element => {
         element.addEventListener('mouseover', handleMousemove);
         element.addEventListener('dblclick', doublClickAction);
@@ -76,6 +135,17 @@ const addEventsToContainer = function(cnt) {
 }
 
 window.addEventListener("load", gridCreate(container, gridSize));
+document.onkeydown = valueOff;
+document.onkeyup = valueOn;
 addEventsToContainer(container);
 resetButton.addEventListener("click", resetAction);
 sizeButton.addEventListener("click", changeSizeAction);
+
+mode.forEach(element => {
+    element.addEventListener('click', selectMode)
+});
+
+containerSize.addEventListener('change', (e) => {
+    removeAll(container);
+    newRestart(e.target.value);
+})
